8e553b209ca42208dc01a4fe7f691f446de60481
brummer10 <brummer-@web.de>
2019-10-01 05:26:16 +0200

    insert guard to prevent event loop

--- gui/gx_CreamMachine_x11ui.c
+++ gui/gx_CreamMachine_x11ui.c
@@ -170,6 +170,7 @@ typedef struct {
 	cairo_t *cr;
 
 	gx_controller controls[CONTROLS];
+	int block_event;
 	gx_scale rescale;
 
 	void *controller;
@@ -250,6 +251,7 @@ static LV2UI_Handle instantiate(const struct _LV2UI_Descriptor * descriptor,
 	ui->controls[2] = (gx_controller) {{0.5, 0.5, 0.0, 1.0, 0.01}, {380, 60, 61, 61}, false,"BASS", KNOB, BASS};
 	ui->controls[3] = (gx_controller) {{0.5, 0.5, 0.0, 1.0, 0.01}, {450, 60, 61, 61}, false,"TREBLE", KNOB, TREBLE};
 	ui->controls[4] = (gx_controller) {{0.5, 0.5, 0.0, 1.0, 0.01}, {520, 60, 61, 61}, false,"VOLUME", KNOB, VOLUME};
+	ui->block_event = -1;
 
 
 	ui->pedal = cairo_image_surface_create_from_stream(ui, LDVAR(pedal_png));
@@ -640,8 +642,10 @@ static void send_controller_event(gx_CreamMachineUI *ui, int controller) {
 static void check_value_changed(gx_CreamMachineUI *ui, int i, float* value) {
 	if(fabs(*(value) - ui->controls[i].adj.value)>=0.00001) {
 		ui->controls[i].adj.value = *(value);
-		ui->write_function(ui->controller,ui->controls[i].port,sizeof(float),0,value);
+		if (ui->block_event != ui->controls[i].port)
+			ui->write_function(ui->controller,ui->controls[i].port,sizeof(float),0,value);
 		send_controller_event(ui, i);
+		ui->block_event = -1;
 	}
 }
 
@@ -1004,6 +1008,7 @@ static void port_event(LV2UI_Handle handle, uint32_t port_index,
 	float value = *(float*)buffer;
 	for (int i=0;i<CONTROLS;i++) {
 		if (port_index == ui->controls[i].port) {
+			ui->block_event = (int)port_index;
 			check_value_changed(ui, i, &value);
 		}
 	}
